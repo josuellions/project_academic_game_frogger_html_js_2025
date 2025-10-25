1b2649f9cf2cf2f258d7fa494deb4264f205736f
josuellions <josuel_lions@hotmail.com>
2025-10-25 19:38:33 -0300

    fix: adjusts remove timeout `gameOver`

--- src/scripts/states/Play.js
+++ src/scripts/states/Play.js
@@ -293,14 +293,14 @@ Play.prototype = {
     sound.play();
 
     //setTimeout(function () {
-    var evt = this.time.events.add(
-      Phaser.Timer.SECOND * 0.5,
-      function () {
-        this.game.state.start("GameOver", true, false, this.points);
-        this.time.events.remove(evt); // remove manualmente
-      },
-      this
-    );
+    // var evt = this.time.events.add(
+    //   Phaser.Timer.SECOND * 0.5,
+    //   function () {
+    this.game.state.start("GameOver", true, false, this.points);
+    //this.time.events.remove(evt); // remove manualmente
+    //   },
+    //   this
+    // );
     //}, 1000);
   },
   collidesWithTreasury: function () {
